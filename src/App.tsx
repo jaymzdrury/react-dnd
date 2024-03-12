99706bd56b4daf0c4ce338bbfc41d451fb6b6f48
James Drury <57101727+jaymzdrury@users.noreply.github.com>
2024-03-12 16:11:48 -0400

    js 2022

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import React from "react";
 const data = [1, 2, 3, 4];
 
 function App() {
-  const [arr, arrSet] = React.useState(data);
+  const [arr, arrSet] = React.useState<number[]>(data);
   const drag = React.useRef(0);
   const draggedOver = React.useRef(0);
+
   const handleDrag = () => {
     const newArr = [...arr];
-    const initialDrag = newArr[drag.current];
-    newArr[drag.current] = newArr[draggedOver.current];
+    const initialDrag = newArr.at(drag.current) as number;
+    newArr[drag.current] = newArr.at(draggedOver.current) as number;
     newArr[draggedOver.current] = initialDrag;
     arrSet(newArr);
   };
+
   return (
     <>
       {arr.map((a, idx) => (
